448627d8829c088c3356e38f332f642a99546462
YangFang86 <yangfang86@outlook.com>
2017-06-13 11:13:17 +0800

    add a new window

--- WpfApp_gittest2/Window1.xaml.cs
+++ WpfApp_gittest2/Window1.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WpfApp_gittest2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 win1 = new Window1();
-            win1.Owner = this;
-            win1.Show();
         }
     }
 }
